e24cf71f8c9cae7cd8dea1f78a5974ef48b470a1
Russell McLoughlin <russ.mcl@gmail.com>
2023-03-30 13:01:20 -0700

    update setup.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ledger_reader",
-    version="0.1",
+    version="0.2",
     author="Russell McLoughlin",
     author_email="russ.mcl@gmail.com",
     description="A simple parser for hledger journal files.",
